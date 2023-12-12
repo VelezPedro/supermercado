bc83b86e5874fdcc630694e3782d0b3e4e4c5deb
JEsposito95 <joaquiesposito@gmail.com>
2023-12-12 18:10:43 -0300

    a ver q obnda

--- src/main/java/com/mycompany/supermecardo/persistencia/ControladorPresistencia.java
+++ src/main/java/com/mycompany/supermecardo/persistencia/ControladorPresistencia.java
@@ -9,7 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-
+ 
 public class ControladorPresistencia {
     
     ProductoJpaController prodJpa = new ProductoJpaController();
