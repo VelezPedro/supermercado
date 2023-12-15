b347e25674fc60c6bfc88def851083e4bff9c833
JEsposito95 <joaquiesposito@gmail.com>
2023-12-15 15:58:23 -0300

    a verr

--- target/generated-sources/annotations/com/mycompany/supermecardo/entidades/Usuario_.java
+++ target/generated-sources/annotations/com/mycompany/supermecardo/entidades/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-12-12T16:59:42", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-12-15T02:03:56", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
